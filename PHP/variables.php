ccfaaa2d4be52abe16580fbbfcd2c19d490ca7db
danielosorio0224 <dosorioquimbayo@gmail.com>
2023-10-24 13:27:34 -0500

    first_commit

---
+++ PHP/variables.php
@@ -0,0 +1,18 @@
+<?php	
+$texto_variable = "Hola";
+//Asignando valor
+$numero = 11;
+$falso = true;
+//CAmbiando valor
+$numero = 25;
+
+echo $numero . $falso ;
+
+$numero = 11;
+echo $numero;
+
+$flotante = 2.55;
+$nulo = null;
+$arreglo = ["Daniel","Osorrio",24];
+
+?>
