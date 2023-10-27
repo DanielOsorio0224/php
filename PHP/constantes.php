ce1c9fc714214de38aa67ba0a3c9955bf2ec7424
danielosorio0224 <dosorioquimbayo@gmail.com>
2023-10-27 16:46:01 -0500

    operadores_y_constantes

---
+++ PHP/constantes.php
@@ -0,0 +1,14 @@
+<?php
+//Constantes definidas por el desarrollador
+define('nombre','Daniel Osorio');
+define('edad', 24);
+
+echo nombre;
+echo edad;
+
+//Constantes predefinidas del propio lenguaje
+echo PHP_OS;
+echo PHP_VERSION;
+echo __LINE__;
+echo __FILE__;
+?>
---
+++ PHP/operadores.php
@@ -0,0 +1,17 @@
+<?php
+$numero1 = 11;
+$numer02 = 8;
+
+echo 'Suma: '.($numero1+$numer02).'<br/>';
+echo 'Resta: '.($numero1-$numer02).'<br/>';
+echo 'Multiplicacion: '.($numero1*$numer02).'<br/>';
+echo 'Division: '.($numero1/$numer02).'<br/>';
+echo 'Resto: '.($numero1%$numer02).'<br/>';
+
+$year = 2023;
+
+echo $year;
+
+$year++;
+$year--;
+?>
