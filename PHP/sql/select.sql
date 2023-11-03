ec5f586bec8e32e8900aec43e358c3f1408c361b
danielosorio0224 <dosorioquimbayo@gmail.com>
2023-11-03 18:13:13 -0500

    mas funciones para select

--- PHP/sql/select.sql
+++ PHP/sql/select.sql
@@ -15,3 +15,22 @@ SELECT RAND() AS 'OPERATION' FROM usuarios;
 SELECT ROUND(7.2563466,2) AS 'OPERATION' FROM usuarios;
 SELECT SQRT(25) AS 'OPERATION' FROM usuarios;
 SELECT TRUNCATE(7.3434,0) AS 'OPERATION' FROM usuarios;
+
+--Funciones para textos
+SELECT UPPER(nombre) FROM usuarios;
+SELECT CONCAT(nombre,' ',apellidos) AS 'conversion' FROM usuarios;
+SELECT LENGTH(nombre) FROM usuarios;
+SELECT TRIM(nombre) FROM usuarios;
+
+--Funciones para fechas
+SELECT email, fecha, CURDATE() AS 'Fecha actual' FROM usuarios;
+SELECT email, DATEIFF(fecha,CURDATE()) AS 'Fecha actual' FROM usuarios;
+SELECT email, DAYNAME(fecha) AS 'Fecha actual' FROM usuarios;
+
+--Funciones generales
+SELECT email, ISNULL(apellidos) FROM usuarios;
+SELECT STRCMP('HOLA','HOLA') FROM usuarios;
+SELECT VERSION() FROM usuarios;
+SELECT USER() FROM usuarios;
+SELECT DATABASE() FROM usuarios;
+SELECT IFNULL(apellidos, 'ESTE CAMPO ESTA VACIO') FROM usuarios;
