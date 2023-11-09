fc37bcd752f0d9937fd72c6ddb6c05dc5d914944
danielosorio0224 <dosorioquimbayo@gmail.com>
2023-11-08 22:06:24 -0500

    mejora_2_css

--- Proyectos/blog/index.php
+++ Proyectos/blog/index.php
@@ -32,6 +32,8 @@
                 </li>
             </ul>
         </nav>
+
+        <div class="clearfix"></div>
     </header>
 
     <div id="container">
@@ -74,13 +76,18 @@
         <div id="principal">
             <h1>Ultimas entradas</h1>
             <article class="entrada">
+                <a href="">
                 <h2>Titulo de la entrada</h2>
                 <p>
                     Texto de la entrada
                 </p>
+                </a>
             </article>
+            <div id="ver_todas">
+            <a href="">Ver todas las entradas</a>
         </div>
-
+        </div>        
+        <div class="clearfix"></div>
     </div>
     <!-- Footer -->
     <footer id="footer">
