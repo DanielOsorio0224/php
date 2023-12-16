7818ef2e02f03080f2dd0a02df806f9d37febf8e
danielosorio0224 <dosorioquimbayo@gmail.com>
2023-12-16 15:06:46 -0500

    modificar imagen

--- librerias/imagenes/index.php
+++ librerias/imagenes/index.php
@@ -5,4 +5,10 @@ require_once '../vendor/autoload.php';
 $foto_original = 'wow.png';
 $guardar_en = 'fotomodificada.png';
 
-$thumb = new PHPThumb\GD('');
+$thumb = new PHPThumb\GD($foto_original);
+$thumb->resize(250,250);
+
+$thumb->crop(50,50,120,120);
+
+$thumb->show();
+$thumb->save($guardar_en);
