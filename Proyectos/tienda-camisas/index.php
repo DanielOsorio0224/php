5d6b78d80c31e5386caca9cd069eac8165742a69
danielosorio0224 <dosorioquimbayo@gmail.com>
2023-12-21 22:08:37 -0500

    HTML_proyecto

---
+++ Proyectos/tienda-camisas/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Tienda de Camisetas</title>
+        <link rel="stylesheet" href="assets/css/styles.css">
+    </head>    
+    <body>
+        <header id="header">
+            <div id="logo">
+                <img src="assets/camisa.jpg" alt="Camisa">
+                <a href="index.php">
+                    Tienda Camisetas
+                </a>
+            </div>
+        </header>
+
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 3</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="content">
+            <aside id="lateral">
+                <div id="login" class="block_aside">
+                    <form action="" method="POST">
+                        <label for="email">Email</label>
+                        <input type="email" name="email">
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password">
+                        <input type="submit" value="Enviar">
+                    </form>
+                    <a href="#">Mis Pedidos</a>
+                    <a href="#">Gestionar Pedidos</a>
+                </div>
+            </aside>
+
+            <div id="central">
+                <div class="product">
+                    <img src="assets/camisa.jpg" alt="camiseta">
+                    <h2>Camisa Negra</h2>
+                    <p>50 dolares</p>
+                    <a href="#">Comprar</a>
+                </div>
+            </div>
+        </div>
+
+        <footer id="footer">
+            <p>Desarrollado por Daniel Osorio &copy; <?=date('Y') ?></p>
+        </footer>
+    </body>    
+</html>    
