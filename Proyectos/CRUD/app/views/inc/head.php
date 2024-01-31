3125ef1a4a411e29b2f4ab1fb72b976f74b117fb
danielosorio0224 <dosorioquimbayo@gmail.com>
2024-01-30 23:01:27 -0500

    contenido_login_404

--- Proyectos/CRUD/app/views/inc/head.php
+++ Proyectos/CRUD/app/views/inc/head.php
@@ -2,6 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= APP_NAME; ?></title>
 <link rel="stylesheet" href="<?=APP_URL?>app/views/styles/bulma.min.css">
-<link rel="stylesheet" href="<?=APP_URL?>app/views/styles/styles.css">
+<link rel="stylesheet" href="<?=APP_URL?>app/views/styles/styles.css?v=1.0">
 <link rel="stylesheet" href="<?=APP_URL?>app/views/styles/sweetalert2.min.css">
 <script src="<?=APP_URL?>app/views/js/sweetalert2.all.min.js"></script>
