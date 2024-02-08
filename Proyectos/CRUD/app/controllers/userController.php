1fd56a73edfc456c87146d8a6f211c82e5c7b778
danielosorio0224 <dosorioquimbayo@gmail.com>
2024-02-08 09:00:35 -0500

    userController

--- Proyectos/CRUD/app/controllers/userController.php
+++ Proyectos/CRUD/app/controllers/userController.php
@@ -68,5 +68,123 @@
                 return json_encode($alerta);
                 exit();
             }
+
+            if($email!=""){
+                if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+                    $check_email=$this->ejecutarConsulta("SELECT usuario_email FROM usuario WHERE usuario_email='$email'");
+
+                    if($check_email->rowCount()>0){
+                        $alerta=[
+                            "tipo"=>"simple",
+                            "titulo"=>"Ocurrio un error",
+                            "texto"=>"El email ya existe",
+                            "icono"=>"error"
+                        ];
+                        return json_encode($alerta);
+                        exit();
+                    }
+                }else{
+                    $alerta=[
+                        "tipo"=>"simple",
+                        "titulo"=>"Ocurrio un error",
+                        "texto"=>"El email es incorrecto",
+                        "icono"=>"error"
+                    ];
+                    return json_encode($alerta);
+                    exit();
+                }
+            }
+
+            if($clave1!=$clave2){
+                $alerta=[
+                    "tipo"=>"simple",
+                    "titulo"=>"Ocurrio un error",
+                    "texto"=>"La claves no coinciden",
+                    "icono"=>"error"
+                ];
+                return json_encode($alerta);
+                exit();
+            }else{
+                $clave=password_hash($clave1,PASSWORD_BCRYPT,["cost"=>10]);
+            }
+
+            $check_usuario = $this->ejecutarConsulta("SELECT usuario_usuario FROM usuario WHERE usuario_usuario='$usuario'");
+            if($check_usuario->rowCount()>0){
+                $alerta=[
+                    "tipo"=>"simple",
+                    "titulo"=>"Ocurrio un error",
+                    "texto"=>"El usuario ya existe",
+                    "icono"=>"error"
+                ];
+                return json_encode($alerta);
+                exit();
+            }
+
+            $img_dir="../views/fotos/";
+
+            if($_FILES['usuario_foto']['name']!="" && $_FILES['usuario_foto']['size']>0){
+                if(file_exists($img_dir)){
+                    if(!mkdir($img_dir,0777)){
+                        $alerta=[
+                            "tipo"=>"simple",
+                            "titulo"=>"Ocurrio un error",
+                            "texto"=>"Error al crear el directorio",
+                            "icono"=>"error"
+                        ];
+                        return json_encode($alerta);
+                        exit();   
+                    }
+                }
+
+                if(mime_content_type($_FILES['usuario_foto']['tmp_name'])!="image/jpeg" ||
+                mime_content_type($_FILES['usuario_foto']['tmp_name'])!="image/png"){
+                    $alerta=[
+                        "tipo"=>"simple",
+                        "titulo"=>"Ocurrio un error",
+                        "texto"=>"El formato de la foto no es valido",
+                        "icono"=>"error"
+                    ];
+                    return json_encode($alerta);
+                    exit();   
+                }
+
+                if(($_FILES['usuario_foto']['size']/1024)>5120){
+                    $alerta=[
+                        "tipo"=>"simple",
+                        "titulo"=>"Ocurrio un error",
+                        "texto"=>"El peso de la foto es muy grande",
+                        "icono"=>"error"
+                    ];
+                    return json_encode($alerta);
+                    exit();   
+                }
+
+                $foto=str_ireplace(" ","_",$nombre);
+                $foto=$foto."_".rand(0,100);
+
+                switch(mime_content_type($_FILES['usuario_foto']['tmp_name'])){
+                    case "image/jpeg":
+                        $foto=$foto.".jpeg";
+                        break;
+                    case "image/png":
+                        $foto=$foto.".png";
+                        break;    
+                }
+
+                chmod($img_dir,0777);
+                if(!move_uploaded_file($_FILES['usuario_foto']['tmp_name'],$img_dir.$foto)){
+                    $alerta=[
+                        "tipo"=>"simple",
+                        "titulo"=>"Ocurrio un error",
+                        "texto"=>"No se pudo subir la foto",
+                        "icono"=>"error"
+                    ];
+                    return json_encode($alerta);
+                    exit();   
+                }
+
+            }else{
+                $foto="";
+            }
         }
     }
