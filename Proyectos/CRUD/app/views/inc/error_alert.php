3b03008fab19005c4aa3fbc9a1297962c64312a6
danielosorio0224 <dosorioquimbayo@gmail.com>
2024-02-13 08:26:23 -0500

    userUpdate

---
+++ Proyectos/CRUD/app/views/inc/error_alert.php
@@ -0,0 +1,7 @@
+
+<article class="message is-danger">
+	<div class="message-header">
+	    <p>¡Ocurrio un error inesperado!</p>
+	 </div>
+    <div class="message-body">No se pudo cargar los datos solicitados</div>
+</article>
